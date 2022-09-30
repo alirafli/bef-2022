e7f5942cf30b53277df60542425b04cd11f8fd2c
alirafli <mrafli1212@gmail.com>
2022-09-30 16:10:28 +0700

    fix object

--- src/components/MedPart/index.jsx
+++ src/components/MedPart/index.jsx
@@ -2,12 +2,37 @@ import React from "react";
 import Text from "../Text";
 import Footer from "../Footer";
 import MOUNTAIN from "../../assets/homepage/mountain.png";
+import OBJECT1 from "../../assets/medPart/object1.png";
+import OBJECT2 from "../../assets/medPart/object2.png";
+import OBJECT3 from "../../assets/medPart/object3.png";
+import OBJECT4 from "../../assets/medPart/object4.png";
+import OBJECT5 from "../../assets/medPart/object5.png";
 
 const MedPart = ({ content }) => {
   return (
-    <div className="bg-gradient-to-b from-transparent to-[#AE92F6]">
-      <div>{content}</div>
-      <div className="relative bg-secondary bg-opacity-75 flex flex-col justify-center items-center h-60 lg:h-[720px] w-10/12 mx-auto rounded-3xl p-24 z-20">
+    <div className="relative bg-gradient-to-b from-transparent to-[#AE92F6] overflow-hidden">
+      <img
+        src={OBJECT1}
+        alt="object1"
+        className="absolute top-10 sm:-top-20 -left-48 sm:-left-60  scale-75 lg:scale-100"
+      />
+      <div className="mb-96">{content}</div>
+      <img
+        src={OBJECT1}
+        alt="object1"
+        className="absolute z-10 top-[87rem] -right-52 sm:-right-72 scale-50 sm:scale-75"
+      />
+      <img
+        src={OBJECT5}
+        alt="object1"
+        className="absolute z-10 top-[110rem] -left-32 sm:-left-20 scale-75 sm:scale-100"
+      />
+      <img
+        src={OBJECT4}
+        alt="object1"
+        className="absolute z-10 top-[70rem] sm:top-[77rem] sm:right-96 scale-50 sm:scale-100"
+      />
+      <div className="relative bg-secondary bg-opacity-75 flex flex-col justify-center items-center h-[720px] w-10/12 mx-auto rounded-3xl p-5 md:p-24 z-20">
         <Text
           variant="h1"
           weight="bold"
@@ -17,7 +42,11 @@ const MedPart = ({ content }) => {
           Calling out sponsors & media partners
         </Text>
       </div>
-      <img src={MOUNTAIN} alt="mountain" className="relative w-screen md:-mt-60   z-10" />
+      <img
+        src={MOUNTAIN}
+        alt="mountain"
+        className="relative w-screen md:-mt-60 z-10"
+      />
       <Footer />
     </div>
   );

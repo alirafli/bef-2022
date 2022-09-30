664365dcdca9ce7a70d9141e9c1507ca3aac2e30
kobulwidodo <fadhilhan01@gmail.com>
2022-09-30 12:59:08 +0700

    feat: add new component for menu widget

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,5 +2,6 @@ import Text from "./Text";
 import Button from "./Button";
 import Navbar from "./Navbar";
 import MedPart from "./MedPart";
+import MenuWidget from "./MenuWidget";
 
-export { Text, Button, Navbar, MedPart };
+export { Text, Button, Navbar, MedPart, MenuWidget };
--- src/pages/Home/Section/sectionThree.jsx
+++ src/pages/Home/Section/sectionThree.jsx
@@ -5,6 +5,7 @@ import EXPO from "../../../assets/homepage/expo.png";
 import COMPETITION from "../../../assets/homepage/competition.png";
 import CATALOG from "../../../assets/homepage/catalog.png";
 import MAP from "../../../assets/homepage/map.png";
+import { MenuWidget } from "../../../components";
 const sectionThree = ({ Text }) => {
   return (
     <div className="bg-whatsOn-bg h-fit lg:h-[800px] bg-contain bg-no-repeat lg:bg-cover lg:bg-center mb-8 px-3 place-content-center overflow-hidden">
@@ -14,84 +15,20 @@ const sectionThree = ({ Text }) => {
         color="white"
         className="text-white mx-auto w-fit pt-20 sm:pt-32 md:pt-44 lg:pt-60"
       >
-        What’s On BEF 2022
+        What's On BEF 2022
       </Text>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5 w-fit mx-auto mt-7">
-        <Tilt
-          options={{ max: 30, scale: 1 }}
-          className="bg-secondary rounded-3xl text-center p-7"
-        >
-          <Text
-            variant="p1"
-            weight="semiBold"
-            color="white"
-            className="text-white mb-2"
-          >
-            Seminar
-          </Text>
-          <img src={SEMINAR} alt="seminar" className="mx-auto w-2/3 md:w-fit" />
-        </Tilt>
-        <Tilt
-          options={{ max: 30, scale: 1 }}
-          className="bg-secondary rounded-3xl text-center p-7 "
-        >
-          <Text
-            variant="p1"
-            weight="semiBold"
-            color="white"
-            className="text-white"
-          >
-            Expo
-          </Text>
-          <img src={EXPO} alt="expo" className="mx-auto w-2/3 md:w-fit" />
-        </Tilt>
-        <Tilt
-          options={{ max: 30, scale: 1 }}
-          className="bg-secondary rounded-3xl text-center p-7"
-        >
-          <Text
-            variant="p1"
-            weight="semiBold"
-            color="white"
-            className="text-white mb-1"
-          >
-            Competition
-          </Text>
-          <img
-            src={COMPETITION}
-            alt="competition"
-            className="mx-auto w-2/3 md:w-fit"
-          />
-        </Tilt>
-        <Tilt
-          options={{ max: 30, scale: 1 }}
-          className="bg-secondary rounded-3xl text-center p-7"
-        >
-          <Text
-            variant="p1"
-            weight="semiBold"
-            color="white"
-            className="text-white mb-1"
-          >
-            Catalog
-          </Text>
-          <img src={CATALOG} alt="catalog" className="mx-auto  my-auto" />
-        </Tilt>
-        <Tilt
-          options={{ max: 30, scale: 1 }}
-          className="bg-secondary rounded-3xl text-center p-7"
-        >
-          <Text
-            variant="p1"
-            weight="semiBold"
-            color="white"
-            className="text-white mb-3"
-          >
-            MAP
-          </Text>
-          <img src={MAP} alt="map" className="mx-auto  my-auto" />
-        </Tilt>
+        <MenuWidget to="#" text="Seminar" image={SEMINAR} alt="seminar" />
+        <MenuWidget to="#" text="Expo" image={EXPO} alt="expo" />
+        <MenuWidget
+          to="#"
+          text="Competition"
+          image={COMPETITION}
+          alt="competition"
+        />
+        <MenuWidget to="#" text="Catalog" image={CATALOG} alt="catalog" />
+        <MenuWidget to="#" text="MAP" image={MAP} alt="map" />
       </div>
     </div>
   );

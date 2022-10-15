ccc4318582a435f1f903150d73403fe626c67675
alirafli <mrafli1212@gmail.com>
2022-10-15 11:35:49 +0700

    fix: navbar route

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -5,16 +5,15 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import logo from "../../assets/logo/logo-mw2.svg";
 
 const navigation = [
-  { name: "Home", href: "/" },
-  { name: "Tentang", href: "/coming-soon" },
-  { name: "MW INFO", href: "/coming-soon" },
-  { name: "Data Pengurus dan Alumni", href: "/coming-soon" },
-  { name: "Contact", href: "coming-soon" },
+  { name: "Home", href: "https://mw.ub.ac.id/" },
+  { name: "Tentang", href: "https://mw.ub.ac.id/tentang/" },
+  { name: "MW INFO", href: "https://mw.ub.ac.id/category/berita/" },
+  {
+    name: "Data Pengurus dan Alumni",
+    href: "https://mw.ub.ac.id/data-pengurus/",
+  },
+  { name: "Contact", href: "https://mw.ub.ac.id/contact-2/" },
 ];
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
 
 const Navbar = () => {
   return (
@@ -55,22 +54,15 @@ const Navbar = () => {
                   <div className="hidden sm:ml-6 sm:block">
                     <div className="flex space-x-4">
                       {navigation.map((item) => (
-                        <NavLink
+                        <a
                           key={item.name}
-                          to={item.href}
+                          href={item.href}
                           smooth="true"
                           duration={500}
-                          className={({ isActive }) => {
-                            return classNames(
-                              isActive
-                                ? " text-orange-600"
-                                : "text-zinc-600 hover:text-orange-600",
-                              "px-3 py-2 rounded-md text-sm font-medium"
-                            );
-                          }}
+                          className="text-zinc-600 hover:text-orange-600 px-3 py-2 rounded-md text-sm font-medium"
                         >
                           {item.name}
-                        </NavLink>
+                        </a>
                       ))}
                     </div>
                   </div>
@@ -81,21 +73,14 @@ const Navbar = () => {
             <Disclosure.Panel className="sm:hidden">
               <div className="space-y-1 px-2 pt-2 pb-3">
                 {navigation.map((item) => (
-                  <NavLink
+                  <a
                     key={item.name}
-                    to={item.href}
-                    className={({ isActive }) => {
-                      return classNames(
-                        isActive
-                          ? " bg-gray-100 text-orange-600"
-                          : "text-zinc-600 hover:bg-gray-100 hover:text-orange-600",
-                        "block px-3 py-2 rounded-md text-base font-medium"
-                      );
-                    }}
+                    href={item.href}
+                    className="text-zinc-600 hover:text-orange-600 px-3 py-2 rounded-md text-sm font-medium"
                     aria-current={item.current ? "page" : undefined}
                   >
                     {item.name}
-                  </NavLink>
+                  </a>
                 ))}
               </div>
             </Disclosure.Panel>

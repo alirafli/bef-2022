3d4f96b0e379ad8996cbd2759401f5800789bd53
alirafli <mrafli1212@gmail.com>
2022-10-25 20:48:07 +0700

    feat: add helmet

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Button, Text, AnimateDiv } from "../../components";
 import IMAGE from "../../assets/not_found.svg";
+import { Helmet } from "react-helmet-async";
 
 const NotFound = () => {
   return (
     <AnimateDiv className="text-center">
+      <Helmet>
+        <title>BEF | Not Found</title>
+      </Helmet>
       <img src={IMAGE} alt="404 not found" className="lg:w-5/12 mx-auto" />
       <Text variant="p2" className="lg:-mt-24">
         <a href="https://storyset.com/web">Web illustrations by Storyset</a>
